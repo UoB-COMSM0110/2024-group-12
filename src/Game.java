67516e7984592731784190d4680f9f6d7a757721
koreanMike513 <paulryu1998@gmail.com>
2024-02-14 18:40:10 +0000

    init: initialised PApllet (Processing) and packages

---
+++ src/Game.java
@@ -0,0 +1,21 @@
+import processing.core.PApplet;
+
+public class Game extends PApplet {
+
+
+  public static void main(String[] args) {
+    PApplet.main("Game");
+  }
+
+  public void settings() {
+
+  }
+
+  public void setup() {
+
+  }
+
+  public void draw() {
+
+  }
+}
